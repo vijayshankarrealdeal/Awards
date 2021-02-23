fb981c186e57c2c0786ada6a70bace1dda9c8304
vijayshankarrealdeal <vijaygaya056@gmail.com>
2021-02-23 07:33:31 +0530

    Allokay

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,7 @@
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'package:voiceApp/app/home/change.dart';
 import 'package:voiceApp/app/home/homescreen.dart';
 
 void main() async {
@@ -13,7 +15,8 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: HomeScreen(),
+      home: ChangeNotifierProvider<ChangeofPage>(
+          create: (context) => ChangeofPage(), child: HomeScreen()),
     );
   }
 }
