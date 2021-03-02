0350643639f309d8415121eea4709e8537b11a75
vijayshankarrealdeal <vijaygaya056@gmail.com>
2021-03-02 19:28:42 +0530

    RingMade

--- lib/app/widgits/ring.dart
+++ lib/app/widgits/ring.dart
@@ -1,15 +1,27 @@
-// import 'package:flutter/cupertino.dart';
+import 'dart:ui';
 
-// class Ring extends CustomPainter {
-//   final _paint = Paint()
-//     ..color = Color.fromRGBO(255, 255, 255, 1)
-//     ..strokeWidth = 2;
+import 'package:flutter/rendering.dart';
 
-//   @override
-//   void paint(Canvas canvas, Size size) {
-//     canvas.drawArc(Rect.fromCenter(center: 20.,_paint));
-//   }
+class Ring extends CustomClipper<Path> {
+  double x;
+  double y;
+  double w;
+  Ring({this.x, this.y, this.w});
 
-//   @override
-//   bool shouldRepaint(CustomPainter oldDelegate) => false;
-// }
+  @override
+  Path getClip(Size size) {
+    var path = Path();
+    var rect = Rect.fromLTRB(0, 0, x, y);
+    path.addOval(rect);
+    path.fillType = PathFillType.evenOdd;
+    var rect2 = Rect.fromLTRB(0 + w, 0 + w, x - w, y - w);
+    path.addOval(rect2);
+    return path;
+  }
+
+  @override
+  bool shouldReclip(CustomClipper<Path> oldClipper) {
+    // TODO: implement shouldReclip
+    return true;
+  }
+}
