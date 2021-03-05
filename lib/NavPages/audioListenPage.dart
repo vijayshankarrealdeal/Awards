2a22cee68d75e2e0abf67bf1f7409d339e6dc9f4
vijayshankarrealdeal <vijaygaya056@gmail.com>
2021-03-06 01:02:30 +0530

    changes

--- lib/NavPages/audioListenPage.dart
+++ lib/NavPages/audioListenPage.dart
@@ -51,7 +51,7 @@ class AudioListenPage extends StatelessWidget {
                     children: [
                       ButtonsforUI(
                         icon: CupertinoIcons.play_arrow_solid,
-                        fontFamily: 'SF-Pro-Text-Regular',
+                        fontFamily: 'SF-Pro-Display-Regular',
                         colorText: Color.fromRGBO(255, 69, 58, 1),
                         radius: 12.0,
                         color: Color.fromRGBO(28, 28, 30, 1),
@@ -63,7 +63,7 @@ class AudioListenPage extends StatelessWidget {
                       ),
                       ButtonsforUI(
                         icon: CupertinoIcons.shuffle,
-                        fontFamily: 'SF-Pro-Text-Regular',
+                        fontFamily: 'SF-Pro-Display-Regular',
                         colorText: Color.fromRGBO(255, 69, 58, 1),
                         radius: 12.0,
                         color: Color.fromRGBO(28, 28, 30, 1),
@@ -118,24 +118,198 @@ class AudioListenPage extends StatelessWidget {
             ),
             SlidingUpPanel(
               collapsed: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
                 children: [
-                  Container(
-                    height: MediaQuery.of(context).size.height * 0.14,
-                    width: MediaQuery.of(context).size.width * 0.14,
-                    decoration: BoxDecoration(
-                      borderRadius: BorderRadius.circular(12),
-                      image: DecorationImage(
-                        image: AssetImage('images/Cover.jpg'),
+                  Row(
+                    children: [
+                      Container(
+                        height: MediaQuery.of(context).size.height * 0.14,
+                        width: MediaQuery.of(context).size.width * 0.14,
+                        decoration: BoxDecoration(
+                          borderRadius: BorderRadius.circular(12),
+                          image: DecorationImage(
+                            image: AssetImage('images/Cover.jpg'),
+                          ),
+                        ),
                       ),
-                    ),
+                      SizedBox(
+                          width: MediaQuery.of(context).size.height * 0.03),
+                      Text(
+                        'A Moment Apart',
+                        style: TextStyle(
+                          color: Color.fromRGBO(255, 255, 255, 1),
+                          fontFamily: 'SF-Pro-Display-Regular',
+                          fontSize: 17,
+                        ),
+                      ),
+                    ],
                   ),
+                  Row(
+                    children: [
+                      IconButton(
+                        icon: Icon(
+                          CupertinoIcons.play_arrow_solid,
+                          color: Color.fromRGBO(255, 255, 255, 1),
+                          size: 28,
+                        ),
+                        onPressed: () => print(''),
+                      ),
+                      IconButton(
+                        icon: Icon(
+                          CupertinoIcons.forward_fill,
+                          color: Color.fromRGBO(255, 255, 255, 1),
+                          size: 28,
+                        ),
+                        onPressed: () => print(''),
+                      ),
+                    ],
+                  )
                 ],
               ),
               minHeight: MediaQuery.of(context).size.height * 0.08,
-              maxHeight: MediaQuery.of(context).size.height * 0.7,
+              maxHeight: MediaQuery.of(context).size.height * 0.89,
               color: Color.fromRGBO(28, 28, 30, 1),
-              panel: Center(
-                child: Text("This is the sliding Widget"),
+              panel: Container(
+                // decoration: BoxDecoration(
+                //     gradient: LinearGradient(colors: [
+
+                //   Colors.blue,
+                //   Colors.blueAccent,
+                //   Colors.purple,
+                // ])),
+                child: Column(
+                  children: [
+                    SizedBox(
+                      height: MediaQuery.of(context).size.height * 0.03,
+                    ),
+                    Icon(
+                      CupertinoIcons.minus,
+                      size: 56,
+                      color: Color.fromRGBO(10, 132, 255, 1),
+                    ),
+                    Padding(
+                      padding: const EdgeInsets.only(left: 20.0, right: 20.0),
+                      child: Container(
+                        decoration: BoxDecoration(
+                            color: Color.fromRGBO(13, 13, 13, 1),
+                            borderRadius: BorderRadius.circular(12.0),
+                            image: DecorationImage(
+                              image: AssetImage(
+                                'images/Cover.jpg',
+                              ),
+                              fit: BoxFit.cover,
+                            )),
+                        height: MediaQuery.of(context).size.height * 0.4,
+                        width: MediaQuery.of(context).size.width * 0.8,
+                      ),
+                    ),
+                    Padding(
+                      padding: const EdgeInsets.only(
+                          left: 18.0, right: 18.0, top: 12, bottom: 22),
+                      child: Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                        crossAxisAlignment: CrossAxisAlignment.center,
+                        children: [
+                          Text(
+                            'A Moment Apart',
+                            style: TextStyle(
+                              color: Color.fromRGBO(255, 255, 255, 1),
+                              fontFamily: 'SF-Pro-Text-Semibold',
+                              fontSize: 32,
+                            ),
+                          ),
+                          IconButton(
+                            icon: Icon(
+                              CupertinoIcons.arrow_2_circlepath,
+                              color: Colors.white,
+                            ),
+                            onPressed: () => print(1),
+                          )
+                        ],
+                      ),
+                    ),
+                    Padding(
+                      padding: const EdgeInsets.all(17.0),
+                      child: Container(
+                        width: double.infinity,
+                        child: CupertinoSlider(
+                            activeColor: Color.fromRGBO(10, 132, 255, 1),
+                            value: 0.0,
+                            onChanged: (onChanged) {
+                              onChanged = 2;
+                            }),
+                      ),
+                    ),
+                    Padding(
+                      padding: const EdgeInsets.only(
+                          left: 18.0, right: 18.0, top: 12),
+                      child: Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                        crossAxisAlignment: CrossAxisAlignment.center,
+                        children: [
+                          IconButton(
+                            icon: Icon(
+                              CupertinoIcons.backward_fill,
+                              color: Colors.white,
+                              size: 35,
+                            ),
+                            onPressed: () => print(1),
+                          ),
+                          IconButton(
+                            icon: Icon(
+                              CupertinoIcons.play_arrow_solid,
+                              color: Colors.white,
+                              size: 35,
+                            ),
+                            onPressed: () => print(1),
+                          ),
+                          IconButton(
+                            icon: Icon(
+                              CupertinoIcons.forward_fill,
+                              color: Colors.white,
+                              size: 35,
+                            ),
+                            onPressed: () => print(1),
+                          ),
+                        ],
+                      ),
+                    ),
+                    Padding(
+                      padding: const EdgeInsets.only(
+                          left: 18.0, right: 18.0, top: 12),
+                      child: Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                        crossAxisAlignment: CrossAxisAlignment.center,
+                        children: [
+                          IconButton(
+                            icon: Icon(
+                              CupertinoIcons.volume_down,
+                              color: Colors.white,
+                              size: 15,
+                            ),
+                            onPressed: () => print(1),
+                          ),
+                          Container(
+                            width: MediaQuery.of(context).size.width * 0.66,
+                            child: CupertinoSlider(
+                                value: 0,
+                                onChanged: (onChanged) {
+                                  print(onChanged);
+                                }),
+                          ),
+                          IconButton(
+                            icon: Icon(
+                              CupertinoIcons.volume_up,
+                              color: Colors.white,
+                              size: 15,
+                            ),
+                            onPressed: () => print(1),
+                          ),
+                        ],
+                      ),
+                    ),
+                  ],
+                ),
               ),
             )
           ],
